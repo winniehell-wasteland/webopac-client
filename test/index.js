99368bb4d685668197fda634eca72d50288c3b57
winniehell <git@winniehell.de>
2016-01-06 22:43:25 +0100

    use mock server in test

--- test/index.js
+++ test/index.js
@@ -1,14 +1,27 @@
 'use strict'
 
-const settings = require('./settings.local.json')
+const testSettings = require('./settings')
 
 const WebOPAC = require('..')
-const client = new WebOPAC(settings.rootUrl)
 
-client.findById(362191)
-  .then(function (title) {
-    console.log(title)
+testSettings.load()
+  .then((settings) => {
+    const client = new WebOPAC(settings.rootUrl)
+
+    return client.findById(362191)
+      .then(function (title) {
+        console.log(title)
+      })
+      .catch(function (e) {
+        console.error(e)
+      })
+      .then(() => {
+        if (settings.mockServer) {
+          settings.mockServer.stop()
+        }
+      })
   })
   .catch(function (e) {
-    console.error(e)
+    const errorLines = e.stack.split('\n')
+    errorLines.forEach(console.error)
   })
